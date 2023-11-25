24d211366249d70520b5b8ffdcf150d3b5b11958
kangsiwoo <bssmkangsiwoo@gmail.com>
2023-11-25 06:00:26 -0800

    REFACTOR :: sums

--- src/components/sums.js
+++ src/components/sums.js
@@ -1,27 +1,18 @@
 import './style/sums.css'
 export default (props) => {
+  let typename = '메신저 요약'
+
   if (props.type==='border') {
-    return (
-      <div id="sum">
-        <h1>공지</h1>
-        <div className={"viewer"}></div>
-      </div>
-    )
+    typename = '공지'
+  } else if (props.type==='event') {
+    typename = '일정'
   }
-  if (props.type==='event') {
-    return (
-      <div id="sum">
-        <h1>일정</h1>
-        <div className={"viewer"}></div>
-      </div>
-    )
-  }
-  if (props.type==='messenger') {
-    return (
-      <div id="sum">
-        <h1>메신저 요약</h1>
-        <div className={"viewer"}></div>
-      </div>
-    )
-  }
+
+  return (
+    <div id="sum">
+      <h1>{typename}</h1>
+      <div className={"viewer"}></div>
+    </div>
+  )
+
 }
