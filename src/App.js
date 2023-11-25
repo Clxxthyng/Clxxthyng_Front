5f62693f558dcfd23ede22960ffbc90bf6bfbd58
kangsiwoo <bssmkangsiwoo@gmail.com>
2023-11-25 01:44:59 -0800

    ADD :: login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import GlobalHeader from "./components/global-header";
+import Login from "./components/login"
 
 function App() {
   return (
     <div id="Main">
       <GlobalHeader />
-
+      <Login />
     </div>
   );
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,8 +1,23 @@
+import './style/login.css'
+import close from '../images/close.svg'
+import invisibility from '../images/visibility_off.svg'
+
 export default () => {
   return (
     <div id={"login"}>
-      <section className={"login"}>
-
+      <section >
+        <form action="#!" className={"login"} method="post">
+          <img src={close} alt="close"/>
+          <h1>Login</h1>
+          <p>클루팅에 오신 것을 환영합니다.</p>
+          <form action="#" method={"post"}></form>
+          <section className={"id"}><input name="id" id={"login-id"} type="id" placeholder="아이디를 입력해주세요."/></section>
+          <section className={"password"}><input name="password" id={"login-password"} type="password" placeholder="비밀번호를 입력해주세요."/><img src={invisibility}
+                                                                                                                                         alt="invisibility"
+                                                                                                                                         className={"invisibility"}/></section>
+          <div className={"autologin"}><div><input type="checkbox" name="autoLogin" id="autoLogin" className={"box"}/></div><span>자동로그인</span></div>
+          <input type="submit" className={"submit"} value={"로그인"}/>
+        </form>
       </section>
     </div>
   )
